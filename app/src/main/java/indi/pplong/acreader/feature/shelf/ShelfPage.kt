9476971f122cc9676a3925d94611f243bb2f0a1f
fanlumaster <1730976608@qq.com>
2025-03-08 02:24:57 +0800

    [feat]: change search bar style

--- app/src/main/java/indi/pplong/acreader/feature/shelf/ShelfPage.kt
+++ app/src/main/java/indi/pplong/acreader/feature/shelf/ShelfPage.kt
@@ -1,11 +1,12 @@
 package indi.pplong.acreader.feature.shelf
 
-import android.content.Intent
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -14,15 +15,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.core.net.toFile
 import androidx.hilt.navigation.compose.hiltViewModel
 import indi.pplong.acreader.core.unzipFile
-import indi.pplong.acreader.feature.shelf.ui.PreviewTripleBookShelf
 import indi.pplong.acreader.feature.shelf.ui.ShelfFAB
 import indi.pplong.acreader.feature.shelf.ui.ShelfSearchBar
 import indi.pplong.acreader.feature.shelf.ui.TripleBookShelf
 import indi.pplong.acreader.feature.shelf.viewmodel.ShelfUiIntent
 import indi.pplong.acreader.feature.shelf.viewmodel.ShelfViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import java.io.File
 
 /**
@@ -59,8 +60,19 @@ fun ShelfPage() {
             }
         }
     }
+    val drawerState =
+        androidx.compose.material3.rememberDrawerState(initialValue = DrawerValue.Closed)
+    val scope = CoroutineScope(Dispatchers.Main)
     Scaffold(
-        topBar = { ShelfSearchBar({}, {}, modifier = Modifier.padding(horizontal = 16.dp)) },
+        topBar = {
+            ShelfSearchBar(
+                {},
+                {},
+                drawerState,
+                scope,
+                modifier = Modifier.padding(horizontal = 6.dp).fillMaxWidth()
+            )
+        },
         floatingActionButton = { ShelfFAB { filePickerLauncher.launch(arrayOf("*/*")) } }
     ) { innerValues ->
         Column(modifier = Modifier.padding(innerValues)) {
